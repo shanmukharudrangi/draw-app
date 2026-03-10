2226fda058958fe396df6eadb0dcebe3f19971de
Shanmukha Rudrangi <shanmukharudrangi@gmail.com>
2026-03-10 20:34:51 +0530

    added jwt auth middleware for http and ws backends

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,21 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
+import "dotenv/config"
 const app=express();
 app.use(express.json());
+
+app.post("/signup",(req,res)=>{
+
+})
+
+app.post("/signin",(req,res)=>{
+    
+})
+
+app.post("/room",(req,res)=>{
+
+})
+
 app.listen(3001,()=>{
     console.log("Server running on port:3001");
 });
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,15 +1,35 @@
 import {WebSocketServer} from "ws";
-
+//import { IncomingMessage } from "http";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import "dotenv/config";
+const JWT_SECRET = process.env.JWT_SECRET || "secret";
 const wss=new WebSocketServer({port:8080});
 
-wss.on("connection",(ws)=>{
+wss.on("connection",(ws,request)=>{
     console.log("Client connected");
-    wss.on("message",(data)=>{
+    const url=request.url;
+    if(!url){
+        ws.close();
+        return;
+    }
+    const queryParams=new URLSearchParams(url.split('?')[1]);
+    const token=queryParams.get('token');
+
+    if(!token){
+        ws.close();
+        return;
+    }
+    const decoded=jwt.verify(token,JWT_SECRET);
+    if(!decoded || !(decoded as JwtPayload).userId){
+        ws.close();
+        return;
+    }
+    ws.on("message",(data)=>{
         console.log("Received:",data.toString());
         ws.send("Hello from server");
     });
 
-    wss.on("close",()=>{
+    ws.on("close",()=>{
         console.log("Client disconnected");
     });
 });
